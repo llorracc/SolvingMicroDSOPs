be56a3e3df91d8860c62837e62e0911298ee1a7f
Christopher Llorracc Carroll <1320319+llorracc@users.noreply.github.com>
2024-05-08 13:32:57 -0400

    Automatize clean macros for SolvingMicroDSOPs-clean.tex

--- reproduce_text_cctwMoM.sh
+++ reproduce_text_cctwMoM.sh
@@ -1,6 +1,5 @@
 #/bin/bash
 pdflatex -interaction=nonstopmode                           -halt-on-error -file-line-error -shell-escape \\newcommand\\UseOption{FromShell}\\input{cctwMoM}
-bibtex -terse    cctwMoM
 pdflatex -interaction=nonstopmode                           -halt-on-error -file-line-error -shell-escape \\newcommand\\UseOption{FromShell}\\input{cctwMoM}
 pdflatex -interaction=nonstopmode                           -halt-on-error -file-line-error -shell-escape \\newcommand\\UseOption{FromShell}\\input{cctwMoM}
 latexmk -c
