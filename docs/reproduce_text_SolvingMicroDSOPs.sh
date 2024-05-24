3b58479e01ce5ee1c94eb2393a895ad5444ce778
llorracc <git@llorracc.org>
2024-05-23 21:18:41 -0400

    Works -- Sync

--- docs/reproduce_text_SolvingMicroDSOPs.sh
+++ docs/reproduce_text_SolvingMicroDSOPs.sh
@@ -1,8 +1,7 @@
 #/bin/bash
 scriptDir="$(realpath $(dirname $0))" ; cd $scriptDir
-pdflatex -interaction=nonstopmode -shell-escape -output-format dvi SolvingMicroDSOPs
-pdflatex -interaction=nonstopmode -shell-escape -output-format dvi SolvingMicroDSOPs
+pdflatex -shell-escape -output-format dvi SolvingMicroDSOPs
 bibtex -terse SolvingMicroDSOPs
-pdflatex -interaction=nonstopmode -shell-escape -output-format dvi SolvingMicroDSOPs
-pdflatex -interaction=nonstopmode -shell-escape -output-format dvi SolvingMicroDSOPs
-make4ht  --utf8 --config SolvingMicroDSOPs.cfg --format html5 SolvingMicroDSOPs "svg            "   "-cunihtf -utf8"
+pdflatex -shell-escape -output-format dvi SolvingMicroDSOPs
+pdflatex -shell-escape -output-format dvi SolvingMicroDSOPs
+rm -f economics.bib; make4ht  --utf8 --config SolvingMicroDSOPs.cfg --format html5 SolvingMicroDSOPs "svg            "   "-cunihtf -utf8"
